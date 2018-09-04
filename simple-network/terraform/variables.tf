b93d3905842a8c1810dbe9e42e8d327288882cd7
Marco Tesch <mtesch@tecracer.de>
2018-09-04 09:59:01 +0200

    Added variables (w. defaults): + availability_zone + tags

--- simple-network/terraform/variables.tf
+++ simple-network/terraform/variables.tf
@@ -3,7 +3,23 @@ variable "region" {
   description = "Region where the Infrastructure should be deployed"
 }
 
+variable "availability_zone" {
+  type        = "string"
+  description = "Availability Zone for the public subnet"
+  default     = "a"
+}
+
 variable "cidr_block" {
   type        = "string"
   description = "CIDR Range to be used for the VPC"
+}
+
+variable "tags" {
+  type        = "map"
+  description = "Tags to be assigned to resources created for the benchmark"
+
+  default = {
+    Created-For = "tRick Benchmark"
+    Created-By  = "Terraform"
+  }
 }
