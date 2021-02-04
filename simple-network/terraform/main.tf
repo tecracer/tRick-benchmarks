6a9b32c7f796e911a86c92972d9c6ea2b1b62ead
Gernot Glawe <gglawe@tecracer.de>
2021-02-04 14:10:42 +0100

    tf 0.14.5

--- simple-network/terraform/main.tf
+++ simple-network/terraform/main.tf
@@ -1,42 +1,42 @@
 provider "aws" {
-  region = "${var.region}"
+  region = var.region
 }
 
 resource "aws_vpc" "vpc" {
-  cidr_block = "${var.cidr_block}"
+  cidr_block = var.cidr_block
 
-  tags = "${var.tags}"
+  tags = var.tags
 }
 
 resource "aws_subnet" "public_subnet" {
-  availability_zone       = "${var.region}${var.availability_zone}"
-  cidr_block              = "${cidrsubnet(var.cidr_block, 8, 0)}"
+  availability_zone       = var.region+var.availability_zone
+  cidr_block              = cidrsubnet(var.cidr_block, 8, 0)
   map_public_ip_on_launch = true
-  vpc_id                  = "${aws_vpc.vpc.id}"
+  vpc_id                  = aws_vpc.vpc.id
 
-  tags = "${var.tags}"
+  tags = var.tags
 }
 
 resource "aws_internet_gateway" "internet_gateway" {
-  vpc_id = "${aws_vpc.vpc.id}"
+  vpc_id = aws_vpc.vpc.id
 
-  tags = "${var.tags}"
+  tags = var.tags
 }
 
 resource "aws_route_table" "public_routetable" {
-  vpc_id = "${aws_vpc.vpc.id}"
+  vpc_id = aws_vpc.vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = "${aws_internet_gateway.internet_gateway.id}"
+    gateway_id = aws_internet_gateway.internet_gateway.id
   }
 
-  tags = "${var.tags}"
+  tags = var.tags
 }
 
 resource "aws_route_table_association" "public_routetable_association" {
-  route_table_id = "${aws_route_table.public_routetable.id}"
-  subnet_id      = "${aws_subnet.public_subnet.id}"
+  route_table_id = aws_route_table.public_routetable.id
+  subnet_id      = aws_subnet.public_subnet.id
 }
 
 resource "aws_security_group" "webserver-sg" {
@@ -59,7 +59,7 @@ resource "aws_security_group" "webserver-sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  vpc_id = "${aws_vpc.vpc.id}"
+  vpc_id = aws_vpc.vpc.id
 
-  tags = "${var.tags}"
+  tags = var.tags
 }
--- simple-network/terraform/variables.tf
+++ simple-network/terraform/variables.tf
@@ -1,23 +1,23 @@
 variable "region" {
-  type        = "string"
+  type        = string
   description = "Region where the Infrastructure should be deployed"
   default = "eu-central-1"
 }
 
 variable "availability_zone" {
-  type        = "string"
+  type        = string
   description = "Availability Zone for the public subnet"
   default     = "a"
 }
 
 variable "cidr_block" {
-  type        = "string"
+  type        = string
   description = "CIDR Range to be used for the VPC"
   default     = "10.0.0.0/16"
 }
 
 variable "tags" {
-  type        = "map"
+  type        = map
   description = "Tags to be assigned to resources created for the benchmark"
 
   default = {
