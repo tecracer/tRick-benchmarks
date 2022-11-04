7643948cd4f129fe5fb94985248d55058eb8f31d
Gernot Glawe <gglawe@tecracer.de>
2022-11-04 15:56:10 +0100

    GO SDK v2
    - Xray

--- serverless-standard/cdk2-lambda-go/app/go.mod
+++ serverless-standard/cdk2-lambda-go/app/go.mod
@@ -5,5 +5,8 @@ go 1.13
 require (
 	github.com/aws/aws-lambda-go v1.13.3
 	github.com/aws/aws-sdk-go v1.25.48
-	golang.org/x/net v0.0.0-20201031054903-ff519b6c9102 // indirect
+	github.com/aws/aws-sdk-go-v2 v1.17.1
+	github.com/aws/aws-sdk-go-v2/config v1.17.10
+	github.com/aws/aws-sdk-go-v2/service/dynamodb v1.17.3
+	github.com/aws/aws-xray-sdk-go v1.7.1 // indirect
 )
--- serverless-standard/cdk2-lambda-go/app/main/main.go
+++ serverless-standard/cdk2-lambda-go/app/main/main.go
@@ -2,19 +2,44 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"log"
 	"os"
 	"time"
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-lambda-go/lambda"
+
 	"github.com/aws/aws-lambda-go/events"
-    "github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-lambda-go/lambda"
+	
+	"github.com/aws/aws-sdk-go-v2/aws"
+	awshttp "github.com/aws/aws-sdk-go-v2/aws/transport/http"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
+
+	"github.com/aws/aws-xray-sdk-go/instrumentation/awsv2"
+	"github.com/aws/aws-xray-sdk-go/xray"
+
+	"github.com/aws/smithy-go"
+
 )
+
+var Client *dynamodb.Client
+
+func init() {
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		panic("unable to load SDK config, " + err.Error())
+	}
+	// Using the Config value, create the DynamoDB client
+	awsv2.AWSV2Instrumentor(&cfg.APIOptions)
+	Client = dynamodb.NewFromConfig(cfg)
+	
+}
 
 // Item holds Dynamodb input
 type Item struct {
-    itemID string `json:"itemID"`
+	itemID string `json:"itemID"`
     time  string `json:"time"`
 }
 
@@ -26,47 +51,72 @@ type MyEvent struct {
 
 // HandleRequest S3 Event
 func handler(ctx context.Context, s3Event events.S3Event) {
+
 	// See https://github.com/aws/aws-lambda-go/tree/master/events
 	// Handle only one event
 	s3input := s3Event.Records[0].S3
 	fmt.Printf("Bucket = %s, Key = %s \n", s3input.Bucket.Name, 
 	s3input.Object.Key)
 
-	putItem(s3input.Object.Key)
+	ctx, sub := xray.BeginSubsegment(ctx, "Dynamo")
+
+	defer sub.Close(nil)
+
+	putItem(ctx,s3input.Object.Key)
 
 }
 
-func putItem(itemID string){
+func putItem(ctx context.Context, itemID string){
 
 	tableName := os.Getenv("TableName")
 
-	sess := session.Must(session.NewSessionWithOptions(session.Options{
-		SharedConfigState: session.SharedConfigEnable,
-	}))
+	
+
+	
+
 
 	// Create DynamoDB client
-	svc := dynamodb.New(sess)
-
+	
 	t := time.Now()
 
 	input := &dynamodb.PutItemInput{
-        Item: map[string]*dynamodb.AttributeValue{
-            "itemID": {
-                S: aws.String(itemID),
+        Item: map[string]types.AttributeValue{
+            "itemID": &types.AttributeValueMemberS{
+                Value: itemID,
 			},
-			"time" : {
-				S: aws.String(t.String()),
+			"time" : &types.AttributeValueMemberS{
+				Value: t.String(),
 			},
         },
         TableName: aws.String(tableName),
     }
   
-	result, err := svc.PutItem(input)
+	result, err := Client.PutItem(ctx,input)
 	if err != nil {
-		fmt.Println("Got error calling PutItem:")
-		fmt.Println(err.Error())
-		os.Exit(1)
-	}
+		// To get a specific API error
+			var notFoundErr *types.ResourceNotFoundException
+			if errors.As(err, &notFoundErr) {
+				log.Printf("scan failed because the table was not found, %v",
+					notFoundErr.ErrorMessage())
+			}
+	
+			// To get any API error
+			var apiErr smithy.APIError
+			if errors.As(err, &apiErr) {
+				log.Printf("scan failed because of an API error, Code: %v, Message: %v",
+					apiErr.ErrorCode(), apiErr.ErrorMessage())
+			}
+	
+			// To get the AWS response metadata, such as RequestID
+			var respErr *awshttp.ResponseError // Using import alias "awshttp" for package github.com/aws/aws-sdk-go-v2/aws/transport/http
+			if errors.As(err, &respErr) {
+				log.Printf("scan failed with HTTP status code %v, Request ID %v and error %v",
+					respErr.HTTPStatusCode(), respErr.ServiceRequestID(), respErr)
+			}
+	
+			return
+			
+		}
 
 
 	fmt.Println("Successfully added " ,result)
